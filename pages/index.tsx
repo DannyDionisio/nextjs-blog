95dff428f103f0c098b039ad88ed7ae9f52ccdeb
Daniela Dionisio <dannyy.dionisio@gmail.com>
2020-07-16 15:48:19 +0100

    Learn next project with Typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,17 @@ import { getSortedPostsData } from "../lib/posts";
 
 import Link from "next/link";
 import Date from "../components/date";
+import { GetStaticProps } from "next";
 
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+}) {
   return (
     <Layout home>
       <Head>
@@ -43,11 +52,11 @@ export default function Home({ allPostsData }) {
   );
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData();
   return {
     props: {
       allPostsData,
     },
   };
-}
+};
